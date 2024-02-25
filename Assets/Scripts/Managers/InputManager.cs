9195bd550c36b5814d1c9b1a8ca59e50658af251
Hyunwoo Moon <113423186+m00nhyunw00@users.noreply.github.com>
2024-02-26 00:23:35 +0900

    ~섹션5-2

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -5,13 +5,31 @@ using UnityEngine;
 
 public class InputManager
 {
-    public Action KeyAction = null; // 키 입력 발생 시, 실행되는 코드	
+    public Action KeyAction = null; // 키 입력 발생 시, 실행되는 코드
+    public Action<Define.MouseEvent> MouseAction = null;
+
+    bool _pressed = false;
 
     public void OnUpdate()
     {
-        if (Input.anyKey == false)  // 키 입력 미발생 , Action 실행하지 않음	
-            return;
-        if (KeyAction != null)  // 키 입력이 발생했고 KeyAction이 정의되어있을 시, Action 실행
+        if (Input.anyKey && KeyAction != null)  // 입력이 발생했고 Action이 정의되어있을 시, Action 실행
             KeyAction.Invoke();
+
+        if (MouseAction != null)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                MouseAction.Invoke(Define.MouseEvent.Press);
+                _pressed = true;
+            }
+            else
+            {
+                if (_pressed)
+                {
+                    MouseAction.Invoke(Define.MouseEvent.Click);
+                    _pressed = false;
+                }
+            }
+        }
     }
 }
